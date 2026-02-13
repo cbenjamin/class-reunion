9ec5da604f6b08349108176daa3fb1df025af897
cbenjamin <cbenjamin1020@gmail.com>
2026-02-12 23:45:32 -0600

    fix missing icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
   <title>{{ $title ?? config('app.name', 'Reunion') }}</title>
 
   @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" referrerpolicy="no-referrer">
+
   @livewireStyles
   @stack('head')
   @auth
